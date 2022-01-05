b01ae3e66faeb69ba94a65f45b3e1f224875a545
tanjilulkarim <tanjilkarim580@gmail.com>
2022-01-05 15:01:35 +0600

    nav section and banner/hero section done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+
+
+//
 const Navbar = ({ toggle }) => {
     return (
         <>
